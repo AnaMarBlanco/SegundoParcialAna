4c1c5abae737906b95ebf6951b7800cd185c1682
panda <panda@DESKTOP-BLRQCTT>
2020-07-06 11:41:15 -1100

    Creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoPacialAna.DAL
+{
+    class Contexto : DbContext
+    {
+        //por donde enviaron eso?
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DATA/Database.db");
+        }
+    }
+}
